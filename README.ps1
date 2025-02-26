403973454e58a79fd56dcf68bd62ad78b6e56a91
DallanK4R <103675864+DallanK4R@users.noreply.github.com>
2025-02-26 20:46:52 +0100

    Update README.ps1

--- README.ps1
+++ README.ps1
@@ -1,3 +1,9 @@
 Set-WinUILanguageOverride -Language zh-CN
 Set-WinUserLanguageList zh-CN -Force
 Restart-Computer
+
+
+
+Set-WinUILanguageOverride -Language en-US
+Set-WinUserLanguageList en-US -Force
+Restart-Computer
